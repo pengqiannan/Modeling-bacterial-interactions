26263b5c8c78ea9ef2179706c626421ce26d560f
pengqiannan <116168351+pengqiannan@users.noreply.github.com>
2024-11-02 14:21:53 +0800

    Add files via upload

---
+++ code/Memote/memote.py
@@ -0,0 +1,11 @@
+$ memote report snapshot --filename "report.html" path/to/model.xml
+# This will generate the performance report as report.html
+
+# memote report for P.gergoviae
+memote report snapshot --filename P.gergoviae.html P.gergoviae.xml
+# memote report for V.alginolyticus
+memote report snapshot --filename V.alginolyticus.html V.alginolyticus.xml
+# memote report for A.hydrophila
+memote report snapshot --filename A.hydrophila.html A.hydrophila.xml
+# memote report for S.putrefaciens
+memote report snapshot --filename S.putrefaciens.html S.putrefaciens.xml
