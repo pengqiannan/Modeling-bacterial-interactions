7ef77b94d2f790c13c51ce7d0d29f8cfe25cc7a9
pengqiannan <116168351+pengqiannan@users.noreply.github.com>
2024-11-02 18:38:45 +0800

    Add files via upload

---
+++ code/Memote/README.md
@@ -0,0 +1,36 @@
+# memote
+
+
+
+### Installation
+```
+pip install memote
+```
+
+### Main function
+
+- `memote.py`:  Evaluate the quality of models by the metabolic model testing suite (MEMOTE).
+
+### Input parameters
+- `--filename`: output filename 
+
+
+### Documentation
+[The genome-scale metabolic model test suite](https://memote.readthedocs.io/)
+
+
+### Other functions
+
+- `excel2SBML.m`: Convert models from excel to SBML format.
+
+- `getDatabaseMappings_update.m`: Return information on known mappings of database entries to model field names, along with additional information about the  fields.
+
+- `makeSBMLAnnotationString_updata.m`: Give the annotationString for an SBML based on the fields in the model.
+
+- `writeSBML_update.m`: Export a COBRA structure into an SBML FBCv2 file. 
+
+
+
+### References
+
+- Lieven, C., Beber, M.E., Olivier, B.G. et al. MEMOTE for standardized genome-scale metabolic model testing. Nat Biotechnol 38, 272–276 (2020). https://doi.org/10.1038/s41587-020-0446-y
---
+++ code/Memote/excel2SBML.m
@@ -0,0 +1,319 @@
+clear
+%%%%%%% P. gergoviae L1
+%% load model
+model = readCbModel('P. gergoviae L1.xls');
+% rxns list
+rxn_list = readtable('P. gergoviae L1.xls');
+% Metabolite List
+met_list = readtable('P. gergoviae L1.xls','Sheet','Metabolite List');
+%% model.metHMDBID:Secondary Accession Numbers : HMDB00902(https://hmdb.ca/metabolites/HMDB0000902)
+for t =1:length(model.metHMDBID)
+    metHMDBID = model.metHMDBID{t,1}
+    if isempty(metHMDBID)
+    model.metHMDBID{t,1} = metHMDBID
+    else
+    metHMDBID(5:6) = [];
+    model.metHMDBID{t,1} = metHMDBID
+    end
+end
+%% add model.metBIGGID
+model.metBiGGID = table2cell(met_list(:,7));
+model.metBiGGID = regexprep(model.metBiGGID, '^BiGG: ', '', 'once');
+%% add model.metSEEDID
+model.metSEEDID = table2cell(met_list(:,8));
+%% MetaNetX(MNX) Chemical
+%% add model.metMetaNetXID
+model.metMetaNetXID = table2cell(met_list(:,9));
+%% inchikey
+%% add model.metInChIString
+model.metInchikey=table2cell(met_list(:,10));
+%% inchi
+%% add model.metInChIString
+model.metInChIString=table2cell(met_list(:,11));
+%% change metChEBIID:  CHEBI:XXX
+model.metChEBIID = table2cell(met_list(:,12));
+model.metChEBIID = strcat('CHEBI: ',model.metChEBIID);
+%% add model.metBioCycID
+model.metBioCycID = table2cell(met_list(:,15));
+%%%%%%%%
+%% add rxnKEGGID
+model.rxnKEGGID= table2cell(rxn_list(:,10));
+%% add rxnseedID
+model.rxnSEEDID=table2cell(rxn_list(:,11));
+%% add rxnRheaID
+rxnRheaID = table2cell(rxn_list(:,12));
+rxnRheaID_t = cellfun(@num2str, rxnRheaID, 'UniformOutput', false);
+for t1 =1:length(rxnRheaID)
+    if ismember(rxnRheaID_t(t1,1),'NaN')
+        model.rxnRheaID(t1,1) = {''};
+    else
+        model.rxnRheaID(t1,1) = rxnRheaID_t(t1,1);
+    end
+end  
+%% add MetaNETID
+model.rxnMetaNetXID= table2cell(rxn_list(:,13));
+%% add rxnBiggID
+model.rxnBiGGID=table2cell(rxn_list(:,14));
+%% add rxnBioCycID
+model.rxnBioCycID= table2cell(rxn_list(:,15));
+%% add SBO Terms
+model.metSBOTerms = repmat({'SBO:0000247'}, length(model.mets), 1);  
+model.rxnSBOTerms = repmat({'SBO:0000176'}, length(model.rxns), 1); 
+r = find(model.c);% Biomass
+model.rxnSBOTerms(r,1) = {'SBO:0000629'}; 
+%%
+model.comps = {'c'; 'e';};  
+model.compNames = {'cytosol' ; 'extracellular';};
+% add model.mets
+for i = 1:length(model.mets)
+    if endsWith(model.mets{i}, '[c0]')
+        model.metComps(i,1) = find(strcmp(model.comps, 'c'));
+    elseif endsWith(model.mets{i}, '[e0]')
+        model.metComps(i,1) = find(strcmp(model.comps, 'e'));
+    else
+        warning('Metabolite %s does not match any known compartments.', model.mets{i});
+    end
+end
+sbmlModel = writeSBML_update(model, 'P.gergoviae');
+% outmodel = writeCbModel(model, 'format','mat');
+
+
+clear
+%%%%%%% V. alginolyticus 45
+%% load model
+model = readCbModel('V. alginolyticus 45.xls');
+% rxns list
+rxn_list = readtable('V. alginolyticus 45.xls');
+% Metabolite List
+met_list = readtable('V. alginolyticus 45.xls','Sheet','Metabolite List');
+%% model.metHMDBID:Secondary Accession Numbers : HMDB00902(https://hmdb.ca/metabolites/HMDB0000902)
+for t =1:length(model.metHMDBID)
+    metHMDBID = model.metHMDBID{t,1}
+    if isempty(metHMDBID)
+    model.metHMDBID{t,1} = metHMDBID
+    else
+    metHMDBID(5:6) = [];
+    model.metHMDBID{t,1} = metHMDBID
+    end
+end
+%% add model.metBIGGID
+model.metBiGGID = table2cell(met_list(:,7));
+model.metBiGGID = regexprep(model.metBiGGID, '^BiGG: ', '', 'once');
+%% add model.metSEEDID
+model.metSEEDID = table2cell(met_list(:,8));
+%% MetaNetX(MNX) Chemical
+%% add model.metMetaNetXID
+model.metMetaNetXID = table2cell(met_list(:,9));
+%% inchikey
+%% add model.metInChIString
+model.metInchikey=table2cell(met_list(:,10));
+%% inchi
+%% add model.metInChIString
+model.metInChIString=table2cell(met_list(:,11));
+%% change metChEBIID:  CHEBI:XXX
+model.metChEBIID = table2cell(met_list(:,12));
+model.metChEBIID = strcat('CHEBI: ',model.metChEBIID);
+%% add model.metBioCycID
+model.metBioCycID = table2cell(met_list(:,15));
+%%%%%%%%
+%% add rxnKEGGID
+model.rxnKEGGID= table2cell(rxn_list(:,10));
+%% add rxnseedID
+model.rxnSEEDID=table2cell(rxn_list(:,11));
+%% add rxnRheaID
+rxnRheaID = table2cell(rxn_list(:,12));
+rxnRheaID_t = cellfun(@num2str, rxnRheaID, 'UniformOutput', false);
+for t1 =1:length(rxnRheaID)
+    if ismember(rxnRheaID_t(t1,1),'NaN')
+        model.rxnRheaID(t1,1) = {''};
+    else
+        model.rxnRheaID(t1,1) = rxnRheaID_t(t1,1);
+    end
+end
+%% add MetaNETID
+model.rxnMetaNetXID= table2cell(rxn_list(:,13));
+%% add rxnBiggID
+model.rxnBiGGID=table2cell(rxn_list(:,14));
+%% add rxnBioCycID
+model.rxnBioCycID= table2cell(rxn_list(:,15));
+%% add SBO Terms
+model.metSBOTerms = repmat({'SBO:0000247'}, length(model.mets), 1);  
+model.rxnSBOTerms = repmat({'SBO:0000176'}, length(model.rxns), 1); 
+r = find(model.c);% Biomass
+model.rxnSBOTerms(r,1) = {'SBO:0000629'}; 
+%%
+model.comps = {'c';'e';};  
+model.compNames = {'cytosol' ;'extracellular';};
+% add model.mets
+for i = 1:length(model.mets)
+    if endsWith(model.mets{i}, '[c0]')
+        model.metComps(i,1) = find(strcmp(model.comps, 'c'));
+    elseif endsWith(model.mets{i}, '[e0]')
+        model.metComps(i,1) = find(strcmp(model.comps, 'e'));
+    else
+        warning('Metabolite %s does not match any known compartments.', model.mets{i});
+    end
+end
+sbmlModel = writeSBML_update(model, 'V.alginolyticus');
+
+
+clear
+%%%%%%% P. gergoviae L1
+%% load model
+model = readCbModel('A. hydrophila 07.xls');
+% rxns list
+rxn_list = readtable('A. hydrophila 07.xls');
+% Metabolite List
+met_list = readtable('A. hydrophila 07.xls','Sheet','Metabolite List');
+%% model.metHMDBID:Secondary Accession Numbers : HMDB00902(https://hmdb.ca/metabolites/HMDB0000902)
+for t =1:length(model.metHMDBID)
+    metHMDBID = model.metHMDBID{t,1}
+    if isempty(metHMDBID)
+    model.metHMDBID{t,1} = metHMDBID
+    else
+    metHMDBID(5:6) = [];
+    model.metHMDBID{t,1} = metHMDBID
+    end
+end
+%% add model.metBIGGID
+model.metBiGGID = table2cell(met_list(:,7));
+model.metBiGGID = regexprep(model.metBiGGID, '^BiGG: ', '', 'once');
+%% add model.metSEEDID
+model.metSEEDID = table2cell(met_list(:,8));
+%% MetaNetX(MNX) Chemical
+%% add model.metMetaNetXID
+model.metMetaNetXID = table2cell(met_list(:,9));
+%% inchikey
+%% add model.metInChIString
+model.metInchikey=table2cell(met_list(:,10));
+%% inchi
+%% add model.metInChIString
+model.metInChIString=table2cell(met_list(:,11));
+%% change metChEBIID:  CHEBI:XXX
+model.metChEBIID = table2cell(met_list(:,12));
+model.metChEBIID = strcat('CHEBI: ',model.metChEBIID);
+%% add model.metBioCycID
+model.metBioCycID = table2cell(met_list(:,15));
+%%%%%%%%
+%% add rxnKEGGID
+model.rxnKEGGID= table2cell(rxn_list(:,10));
+%% add rxnseedID
+model.rxnSEEDID=table2cell(rxn_list(:,11));
+%% add rxnRheaID
+rxnRheaID = table2cell(rxn_list(:,12));
+rxnRheaID_t = cellfun(@num2str, rxnRheaID, 'UniformOutput', false);
+for t1 =1:length(rxnRheaID)
+    if ismember(rxnRheaID_t(t1,1),'NaN')
+        model.rxnRheaID(t1,1) = {''};
+    else
+        model.rxnRheaID(t1,1) = rxnRheaID_t(t1,1);
+    end
+end  
+%% add MetaNETID
+model.rxnMetaNetXID= table2cell(rxn_list(:,13));
+%% add rxnBiggID
+model.rxnBiGGID=table2cell(rxn_list(:,14));
+%% add rxnBioCycID
+model.rxnBioCycID= table2cell(rxn_list(:,15));
+%% add SBO Terms
+model.metSBOTerms = repmat({'SBO:0000247'}, length(model.mets), 1);  
+model.rxnSBOTerms = repmat({'SBO:0000176'}, length(model.rxns), 1); 
+r = find(model.c);% Biomass
+model.rxnSBOTerms(r,1) = {'SBO:0000629'}; 
+%%
+model.comps = {'c'; 'e';};  
+model.compNames = {'cytosol' ; 'extracellular';};
+% add model.mets
+for i = 1:length(model.mets)
+    if endsWith(model.mets{i}, '[c0]')
+        model.metComps(i,1) = find(strcmp(model.comps, 'c'));
+    elseif endsWith(model.mets{i}, '[e0]')
+        model.metComps(i,1) = find(strcmp(model.comps, 'e'));
+    else
+        warning('Metabolite %s does not match any known compartments.', model.mets{i});
+    end
+end
+sbmlModel = writeSBML_update(model, 'A.hydrophila');
+% outmodel = writeCbModel(model, 'format','mat');
+
+
+clear
+%%%%%%% S. putrefaciens 63
+%% load model
+model = readCbModel('S. putrefaciens 63.xls');
+% rxns list
+rxn_list = readtable('S. putrefaciens 63.xls');
+% Metabolite List
+met_list = readtable('S. putrefaciens 63.xls','Sheet','Metabolite List');
+%% model.metHMDBID:Secondary Accession Numbers : HMDB00902(https://hmdb.ca/metabolites/HMDB0000902)
+for t =1:length(model.metHMDBID)
+    metHMDBID = model.metHMDBID{t,1}
+    if isempty(metHMDBID)
+    model.metHMDBID{t,1} = metHMDBID
+    else
+    metHMDBID(5:6) = [];
+    model.metHMDBID{t,1} = metHMDBID
+    end
+end
+%% add model.metBIGGID
+model.metBiGGID = table2cell(met_list(:,7));
+model.metBiGGID = regexprep(model.metBiGGID, '^BiGG: ', '', 'once');
+%% add model.metSEEDID
+model.metSEEDID = table2cell(met_list(:,8));
+%% MetaNetX(MNX) Chemical
+%% add model.metMetaNetXID
+model.metMetaNetXID = table2cell(met_list(:,9));
+%% inchikey
+%% add model.metInChIString
+model.metInchikey=table2cell(met_list(:,10));
+%% inchi
+%% add model.metInChIString
+model.metInChIString=table2cell(met_list(:,11));
+%% change metChEBIID:  CHEBI:XXX
+model.metChEBIID = table2cell(met_list(:,12));
+model.metChEBIID = strcat('CHEBI: ',model.metChEBIID);
+%% add model.metBioCycID
+model.metBioCycID = table2cell(met_list(:,15));
+%%%%%%%%
+%% add rxnKEGGID
+model.rxnKEGGID= table2cell(rxn_list(:,10));
+%% add rxnseedID
+model.rxnSEEDID=table2cell(rxn_list(:,11));
+%% add rxnRheaID
+rxnRheaID = table2cell(rxn_list(:,12));
+rxnRheaID_t = cellfun(@num2str, rxnRheaID, 'UniformOutput', false);
+for t1 =1:length(rxnRheaID)
+    if ismember(rxnRheaID_t(t1,1),'NaN')
+        model.rxnRheaID(t1,1) = {''};
+    else
+        model.rxnRheaID(t1,1) = rxnRheaID_t(t1,1);
+    end
+end
+%% add MetaNETID
+model.rxnMetaNetXID= table2cell(rxn_list(:,13));
+%% add rxnBiggID
+model.rxnBiGGID=table2cell(rxn_list(:,14));
+%% add rxnBioCycID
+model.rxnBioCycID= table2cell(rxn_list(:,15));
+%% add SBO Terms
+model.metSBOTerms = repmat({'SBO:0000247'}, length(model.mets), 1);  
+model.rxnSBOTerms = repmat({'SBO:0000176'}, length(model.rxns), 1); 
+r = find(model.c);% Biomass
+model.rxnSBOTerms(r,1) = {'SBO:0000629'}; 
+%%
+model.comps = {'c' ;'e';}; 
+model.compNames = {'cytosol' ; 'extracellular';};
+% add model.mets
+for i = 1:length(model.mets)
+    if endsWith(model.mets{i}, '[c0]')
+        model.metComps(i,1) = find(strcmp(model.comps, 'c'));
+    elseif endsWith(model.mets{i}, '[e0]')
+        model.metComps(i,1) = find(strcmp(model.comps, 'e'));
+    else
+        warning('Metabolite %s does not match any known compartments.', model.mets{i});
+    end
+end
+sbmlModel = writeSBML_update(model, 'S.putrefaciens');
+% outmodel = writeCbModel(model, 'format','mat');
+
+
